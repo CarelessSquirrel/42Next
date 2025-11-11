4b2681e8200458822fa15d5dfa0f2d5409cfb20c
Jacob Betting <jabettin@f1r2s21.codam.nl>
2025-11-11 16:52:47 +0100

    getnext

---
+++ getnextline/get_next_line.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jabettin <jabettin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 14:16:55 by jabettin          #+#    #+#             */
+/*   Updated: 2025/11/11 14:50:16 by jabettin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+	int	bytes_read;
+	char	*buffer;
+	
+	buffer = malloc(sizeof(char) * 5 + 1);
+	if (!buffer)
+	{
+		return NULL;
+	}
+	bytes_read = read(fd, buffer, 5);
+	if (bytes_read <= 0)
+	{
+		return NULL;
+	}
+	return buffer;
+}
+
+int	main(void)
+{
+	int fd;
+	char *next_line;
+	int count;
+
+	count = 0;
+	fd = open("example.txt", O_RDONLY);
+	while (1)
+	{
+		next_line = get_next_line(fd);
+		if (next_line == NULL)
+		{
+			break;
+		}
+		count++;
+		printf("%d: %s\n", count, next_line);
+		next_line = NULL;
+	}
+	close (fd);
+	return 0;
+}
