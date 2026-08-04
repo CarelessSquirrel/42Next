33b621000690b97bc5a23c2a72f4ab9ca4dbb47d
Jacob Betting <jacobahbetting@gmail.com>
2026-08-04 18:46:50 +0200

    crewmember done

--- Python_Modules/Python_Module09/ex2/space_crew.py
+++ Python_Modules/Python_Module09/ex2/space_crew.py
@@ -15,4 +15,14 @@ class Rank(str, Enum):
 
 
 class CrewMember(BaseModel):
-    member_id
+    member_id: str = Field(..., min_length=3, max_length=10)
+    name: str = Field(..., min_length=2, max_length=50)
+    rank: Rank
+    age: int = Field(..., ge=18, le=80)
+    specialization: str = Field(..., min_length=3, max_length=30)
+    years_experience: int =Field(..., ge=0, le=50)
+    is_active: bool = True
+
+
+class SpaceMission(BaseModel):
+    mission
