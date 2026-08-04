f275a4c7c99107d52867d6163cafe96370aba083
Jacob Betting <jacobahbetting@gmail.com>
2026-08-04 17:49:59 +0200

    intermediary

--- Python_Modules/Python_Module09/ex2/space_crew.py
+++ Python_Modules/Python_Module09/ex2/space_crew.py
@@ -4,3 +4,15 @@ from datetime import datetime
 from pydantic import BaseModel, Field, model_validator, ValidationError, 
 from enum import Enum
 from typing import Optional
+
+
+class Rank(str, Enum):
+    CADET="cadet"
+    OFFICER="officer"
+    LIEUTENANT="lieutenant"
+    CAPTAIN="captain"
+    COMMANDER="commander"
+
+
+class CrewMember(BaseModel):
+    member_id
