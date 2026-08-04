f4d46cb25d43951e584bc340c6c2626be7f6c66b
Jacob Betting <jacobahbetting@gmail.com>
2026-08-04 17:25:07 +0200

    ex2 created

---
+++ Python_Modules/Python_Module09/ex2/space_crew.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from pydantic import BaseModel, Field, model_validator, ValidationError, 
+from enum import Enum
+from typing import Optional
