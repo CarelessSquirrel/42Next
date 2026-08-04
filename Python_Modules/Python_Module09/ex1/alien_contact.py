9834ac710688fc0bf06e0ff8e205314d6503162e
Jacob Betting <jacobahbetting@gmail.com>
2026-08-04 16:51:28 +0200

    ex1 created

---
+++ Python_Modules/Python_Module09/ex1/alien_contact.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, model_validator, Field, ValidationError
+from enum import Enum
+
