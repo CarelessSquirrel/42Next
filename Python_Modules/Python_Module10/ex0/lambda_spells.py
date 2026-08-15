d9c729626d81cebaf1c14d1c5d42e165dda4f526
Jacob Betting <jacobahbetting@gmail.com>
2026-08-16 00:08:07 +0200

    intermediary commit, early sleep

--- Python_Modules/Python_Module10/ex0/lambda_spells.py
+++ Python_Modules/Python_Module10/ex0/lambda_spells.py
@@ -4,9 +4,18 @@ artifacts_list = [
     {'name': 'Crystal Orb', 'power': 85, 'type': 'Magic'}
 ]
 
+mage_dict = [
+    {'name': 'John', 'power': 77, 'element': 'Water'},
+    {'name': 'Claire', 'power': 83, "element": 'Fire'}
+]
+
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
     descending = sorted(artifacts, key=lambda a: a['power'], reverse=True)
     return descending
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    filtered = filter(lambda x: if x >= min_power, x['power'])
 
 
 def main() -> None:
