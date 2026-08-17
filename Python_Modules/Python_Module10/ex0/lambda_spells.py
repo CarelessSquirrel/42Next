8dea348fa9b4c924dc4f35bd25d836e97c58daa0
Jacob Betting <jacobahbetting@gmail.com>
2026-08-17 17:11:43 +0200

    mage_stats wired up

--- Python_Modules/Python_Module10/ex0/lambda_spells.py
+++ Python_Modules/Python_Module10/ex0/lambda_spells.py
@@ -22,22 +22,39 @@ def artifact_sorter(artifacts: list[dict]) -> list[dict]:
 
 
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
-    filtered = list(filter(lambda x: x['power'] >= min_power, mages))
+    filtered = list(filter(lambda m: m['power'] >= min_power, mages))
     return filtered
 
 
 def spell_transformer(spells: list[str]) -> list[str]:
-    transformed = list(map(lambda x: "* " + x + " *", spells))
+    transformed = list(map(lambda s: "* " + s + " *", spells))
     return transformed
 
+
+def mage_stats(mages: list[dict]) -> dict:
+    ranked_max = max(mages, key=lambda m: m['power'])['power']
+    ranked_min = min(mages, key=lambda m: m['power'])['power']
+    ranked_avg = round(sum(map(lambda a: a['power'], mages)) / len(mages), 2)
+    return {
+        'max_power': ranked_max,
+        'min_power': ranked_min,
+        'avg_power': ranked_avg,
+    }
+
 def main() -> None:
+    print("Lambda Sanctum")
+    print("=" * 40)
     sa = artifact_sorter(artifacts)
-    print("Testing artifact sorter...")
-    print(f"{sa[0]['name']} ({sa[0]['power']} power) comes before {sa[1]['name']} ({sa[1]['power']} power)")
+    print("Testing artifact sorter:")
+    print(f"{sa[0]['name']} ({sa[0]['power']} power) comes before {sa[1]['name']} ({sa[1]['power']} power)\n")
     fm = power_filter(mages,40)
-    print("Filtering mages who have more than the mininum amount power")
+    print("Filtering mages who have more than the mininum amount power:")
     for mage in fm:
         print(f"{mage['name']} ({mage['power']} power)")
+    print()
     ts = spell_transformer(spells)
+    print("Testing spell transformation:")
     print(" ".join(ts))
+    rm = mage_stats(mages)
+    print(rm['max_power'])
 main()
