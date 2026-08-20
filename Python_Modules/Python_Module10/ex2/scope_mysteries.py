ec493a9723f6a426934551330e7a6048483a0ccc
Jacob Betting <jacobahbetting@gmail.com>
2026-08-20 18:59:27 +0200

    intermediary

--- Python_Modules/Python_Module10/ex2/scope_mysteries.py
+++ Python_Modules/Python_Module10/ex2/scope_mysteries.py
@@ -20,20 +20,39 @@ def spell_accumulator(initial_power: int) -> Callable:
     return accumulate
 
 
+def enchantment_factory(enchantment_type: str) -> Callable:
+    def apply(item:str) -> str:
+        return f"{enchantment_type} {item}"
+    return apply
+
+
+def memory_vault() -> dict[str, Callable]:
+    def store(key, value):
+       return f"'{key}' '{value}'"
+    return store
+
+    def recall(key):
+        ...
+
+
 def main():
     print("Testing mage counter:")
     counter_a = mage_counter()
     for call_num in range(1, 3):
         print(f"counter_a call: {call_num}: {counter_a()}")
     counter_b = mage_counter()
-    print(f"counter_b call 1: {counter_b()}")\
+    print(f"counter_b call 1: {counter_b()}")
     print()
     spell_acc = spell_accumulator(100)
     print("Testing spell accumulator:")
     print(f"Base 100, add 20: {spell_acc(20)}")
     print(f"Base 100, add 30: {spell_acc(30)}")
     print()
-    
-
+    flame_enchant = enchantment_factory("Flaming")
+    frost_enchant = enchantment_factory("Frozen")
+    print(flame_enchant("Sword"))
+    print(frost_enchant("Shield"))
+    store_mem = memory_vault()
+    print(store_mem('secret', 42))
 
 main()
