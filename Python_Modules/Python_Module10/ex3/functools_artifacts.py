3d7ea266826fe3fdcb960dc820bb41bc706098c0
Jacob Betting <jacobahbetting@gmail.com>
2026-08-22 12:38:37 +0200

    start of ex3

---
+++ Python_Modules/Python_Module10/ex3/functools_artifacts.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def spell_recucer(spells: list[int], operations: str) -> int:
+    ...
+
+
+def partial_enchancer(base_enchantment: Callable) -> dict[str, Callable]:
+    ...
+
+
+def memoized_fibonacci(n: int) -> int:
+    ...
+
+
+def spell_dispatcher() -> Callable[[Any], str]:
+    ...
