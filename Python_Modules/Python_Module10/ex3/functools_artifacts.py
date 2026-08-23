ad59d20521d4c5eec82004358caa2f62f6d4c3c5
Jacob Betting <jacobahbetting@gmail.com>
2026-08-23 23:02:58 +0200

    think its done

--- Python_Modules/Python_Module10/ex3/functools_artifacts.py
+++ Python_Modules/Python_Module10/ex3/functools_artifacts.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-
+import functools
 from collections.abc import Callable
 from typing import Any
+import operator
 
 
-def spell_recucer(spells: list[int], operations: str) -> int:
-    ...
+operation_map = {
+    'add': operator.add,
+    'multiply': operator.mul,
+    'max': max,
+    'min': min,
+}
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+    if not spells:
+        return 0
+    if operation not operation_map:
+        raise ValueError(f"Unknown operation: {operation}")
+    return functools.reduce(operation_map[operation], spells)
+
+
 
 
 def partial_enchancer(base_enchantment: Callable) -> dict[str, Callable]:
